758e82965b0ed42139772f6837ad542ba29cd685
GastroGeek <gastrogeek@gmail.com>
2019-07-19 20:49:28 +0100

    Remove commented-out code

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,11 +1,5 @@
 module.exports = {
   title: 'VuePress + Tailwindcss',
-  // configureWebpack(cfg) {
-  //   cfg.plugins.push(require('tailwindcss'))
-  //   cfg.plugins.push(require('autoprefixer'))
-
-  //   cfg.resolve.alias['tailwindcss'] = 'tailwindcss'
-  // }
   postcss: {
     plugins: [require('tailwindcss')('./tailwind.config.js'), require('autoprefixer')],
   },
